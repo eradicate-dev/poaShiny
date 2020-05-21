529235554f2820cbd45cd56e10bd4e28838a6efc
Simon Howard <howards@landcareresearch.co.nz>
2020-05-21 19:08:02 +1200

    Add package specs to app.R file. Needed to detect R packages when deploying to shinyapps

--- app.R
+++ app.R
@@ -1,5 +1,17 @@
 
+# packages ----------------------------------------------------------------
 
+# devtools::install_github("rstudio/reticulate")
+library(shiny)
+library(reticulate)
+library(rgdal)
+library(raster)
+library(kableExtra)
+library(leaflet)
+library(sf)
+library(DT)
+
+# source('https://raw.githubusercontent.com/eradicate-dev/poaShiny/git-sourced-app/app_lines.R')
 source("app_lines.R")
 
 # Run the application 
