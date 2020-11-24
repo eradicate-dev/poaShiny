4b3b0ef78a6888f40efb6558f18b445dad16a1a2
Simon Howard <HowardS@landcareresearch.co.nz>
2020-11-24 16:53:07 +1300

    Remove unused elements from DT datatable object

--- app_lines.R
+++ app_lines.R
@@ -391,7 +391,7 @@ server <- function(input, output, session) {
   
   
   deviceUI <- reactive({
-    dtOut <- DT::datatable(set.animal.params(), editable = TRUE)
+    dtOut <- DT::datatable(set.animal.params(), editable = TRUE, options = list(ordering=F, searching = F, paging = F))
     return(dtOut)
   })
   
