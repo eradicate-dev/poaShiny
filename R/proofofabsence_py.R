53c419709b30d3df5c2709c07c45e1cacd347e87
Simon Howard <HowardS@landcareresearch.co.nz>
2021-04-21 13:59:48 +1200

    Update conda environment package lists

--- R/proofofabsence_py.R
+++ R/proofofabsence_py.R
@@ -30,11 +30,12 @@ conda_env_build_full <- function(envname = "proofofabsence", force.overwrite = T
   # required python packages and versions
   paks <- c("python==3.7.1","numba==0.48.0","numpy==1.17.5","rios==1.4.10","gdal==2.3.2",
             "r==3.*", "rstudio","r-leaflet==2.0.3","r-shiny==1.4.0","r-reticulate==1.14",
-            "r-rgdal==1.4_7","r-raster==3.0_7","r-sf==0.8_0","r-kableextra==1.1.0")
+            "r-rgdal==1.4_7","r-raster==3.0_7","r-sf==0.8_0","r-kableextra==1.1.0", 
+            "r-devtools", "rtools")
 
   message(sprintf("Creating '%s' conda environment - this takes a while ....", envname))
   # create the 'envname' conda environment
-  reticulate::conda_create(envname = envname, packages = paks, forge = T, channel = "conda-forge")
+  reticulate::conda_create(envname = envname, packages = paks, forge = TRUE, channel = "conda-forge")
 
   if(conda_env_check()) message(sprintf("'%s' conda environment built", envname))
 
@@ -50,11 +51,12 @@ conda_env_build_min <- function(envname = "proofofabsence", force.overwrite = TR
   # required python packages and versions
   paks <- c("python==3.7.1","numba==0.48.0","numpy==1.17.5","r==3.*", "rstudio",
             "r-leaflet==2.0.3","r-shiny==1.4.0","r-reticulate==1.14",
-            "r-rgdal==1.4_7","r-raster==3.0_7","r-sf==0.8_0","r-kableextra==1.1.0")
+            "r-rgdal==1.4_7","r-raster==3.0_7","r-sf==0.8_0","r-kableextra==1.1.0",
+            "r-devtools", "rtools")
 
   message(sprintf("Creating '%s' conda environment - this takes a while ....", envname))
   # create the 'envname' conda environment
-  reticulate::conda_create(envname = envname, packages = paks, forge = T, channel = "conda-forge")
+  reticulate::conda_create(envname = envname, packages = paks, forge = TRUE, channel = "conda-forge")
 
   if(conda_env_check()) message(sprintf("'%s' conda environment built", envname))
 
